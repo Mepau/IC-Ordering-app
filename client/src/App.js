2e987d6170e3a7d932ca5448ba852733dcbdf42b
unknown <michaelperezabreu@gmail.com>
2019-10-24 13:30:07 -0700

    Included Redux dependencies to client side and added redux boiler plate

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,21 @@ import React, { Component } from 'react';
 import AppNavbar from './components/AppNavbar';
 import ShopList from './components/ShopList';
 
+import { Provider } from 'react-redux';
+import store from './store';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <Provider store = {store}>
       <div className="App">
         <AppNavbar/>
         <ShopList/>
       </div>
+      </Provider>
     );
   }
 }
