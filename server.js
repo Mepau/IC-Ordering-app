82ad5b2b3eed966eb006dbc910b5a91a5063d13e
unknown <michaelperezabreu@gmail.com>
2019-10-25 15:12:22 -0700

    Deployment ready

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const items = require('./routes/API/items');
 const orders = require('./routes/API/order');
@@ -24,6 +25,18 @@ mongoose
     //app.use('/API/items', items);
     app.use('/API/items', items);
 
+    //Attend to server when in production
+    if(process.env.NODE_ENV === 'production') {
+        //Set static folder
+        app.use(express.static('client/build'));
+
+        app.get('*', (req, res) => {
+            res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        });
+    }
+
+
+    //Port
     const port = process.env.PORT || 5000;
 
     app.listen(port, () => console.log(`Service started port: ${port}`));
