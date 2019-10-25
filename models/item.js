b1e5d3cb3639e8d35d1df381a0ab2b18a3c60990
unknown <michaelperezabreu@gmail.com>
2019-10-25 15:56:52 -0700

    make it better

--- models/item.js
+++ models/item.js
@@ -14,29 +14,5 @@ const itemSchema = Schema({
 
 });
 
-const orderSchema = Schema({
-    description: {
-        type: String,
-        required: true
-    },
-    price: {
-        type: Number,
-        default: null
-    },
-    id: {
-        type: String,
-        required: true
-    },
-    payMethod: {
-        type: String,
-        default: 'Efectivo'
-    },
-    payed:{
-        type: Boolean,
-        default: false
-    }
-
-
-});
 
 module.exports = item = mongoose.model('item', itemSchema);
